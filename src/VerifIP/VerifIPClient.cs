8337188a70d5d053bef04f0952b027bdd861f782
Nihit <nihit@bipolarfactory.com>
2026-05-10 15:17:11 +0530

    Release v0.2.0: bump all SDK versions to 0.2.0

--- src/VerifIP/VerifIPClient.cs
+++ src/VerifIP/VerifIPClient.cs
@@ -19,7 +19,7 @@ namespace VerifIP;
 /// </example>
 public sealed class VerifIPClient : IDisposable
 {
-    private const string SdkVersion = "0.1.4";
+    private const string SdkVersion = "0.2.0";
     private static readonly int[] RetryableStatusCodes = { 429, 500, 502, 503, 504 };
 
     private readonly HttpClient _httpClient;
